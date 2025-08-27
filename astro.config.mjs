b2487e6fe856232219bfedb8c0634eb9388e4afd
Raullopez20 <raullopez20r@gmsil.com>
2025-08-27 12:00:51 +0200

    Actualización de configuración de sitio en astro.config.mjs: cambio de URL para despliegue en Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,8 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from "@astrojs/sitemap";
 
-// Configuración para GitHub Pages
-// Cambia 'barrio-fino' por el nombre de tu repositorio
 export default defineConfig({
-    site: 'https://Raullopez20.github.io',
+    site: 'https://barriofino.netlify.app/barriofino_web',
     base: '/barriofino_web/',
     output: 'static',
     integrations: [
